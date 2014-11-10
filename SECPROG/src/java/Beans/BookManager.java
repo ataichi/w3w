57272ce2795acfccd25ff10e21921f264e65a700
Danica Christine Corpuz <dccorpuz1@yahoo.com>
2014-11-10 11:08:13 +0800

    book manager bean

---
+++ SECPROG/src/java/Beans/BookManager.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author Danica
+ */
+public class BookManager {
+    private int bookmanagerID;
+    private int bookmngr_prodmngrID;
+
+    /**
+     * @return the bookmanagerID
+     */
+    public int getBookmanagerID() {
+        return bookmanagerID;
+    }
+
+    /**
+     * @param bookmanagerID the bookmanagerID to set
+     */
+    public void setBookmanagerID(int bookmanagerID) {
+        this.bookmanagerID = bookmanagerID;
+    }
+
+    /**
+     * @return the bookmngr_prodmngrID
+     */
+    public int getBookmngr_prodmngrID() {
+        return bookmngr_prodmngrID;
+    }
+
+    /**
+     * @param bookmngr_prodmngrID the bookmngr_prodmngrID to set
+     */
+    public void setBookmngr_prodmngrID(int bookmngr_prodmngrID) {
+        this.bookmngr_prodmngrID = bookmngr_prodmngrID;
+    }
+    
+}
