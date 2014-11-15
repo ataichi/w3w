7fd6df5be454b774a69bc8bfee73da457d5512b3
giodeealbayda <giodeelyn_albayda@dlsu.edu.ph>
2014-11-16 06:54:00 +0800

    Add Product (Not yet finished)

--- WebApplication1/src/java/Servlet/AddProductServlet.java
+++ WebApplication1/src/java/Servlet/AddProductServlet.java
@@ -6,8 +6,11 @@
 package Servlet;
 
 import Beans.AccountBean;
+import Beans.AudioCDBean;
 import Beans.ProductBean;
 import Beans.ProductManagerBean;
+import DAO.Implementation.ProductManagerDAOImplementation;
+import DAO.Interface.ProductManagerDAOInterface;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -40,15 +43,47 @@ public class AddProductServlet extends HttpServlet {
             /* TODO output your page here. You may use following sample code. */
 
             HttpSession session = request.getSession();
-            AccountBean account = new AccountBean();
-            ProductManagerBean productManager = (ProductManagerBean) session.getAttribute("homeproduct");
+            AccountBean homeproduct = (AccountBean) session.getAttribute("homeproduct");
+            ProductManagerBean productManager = new ProductManagerBean();
+            ProductManagerDAOInterface pdao = new ProductManagerDAOImplementation();
+            productManager = pdao.getProductManagerBeanById(homeproduct.getAccountID());
 
             ProductBean product = new ProductBean();
             String type, title, summary, genre;
             double price;
             int year, numberStocks;
+
+            type = productManager.getProdType();
+            title = request.getParameter("productTitle");
+            summary = request.getParameter("productSummary");
+            genre = request.getParameter("productGenre");
+            price = Double.parseDouble(request.getParameter("productPrice"));
+            year = Integer.parseInt(request.getParameter("productYear"));
+            numberStocks = Integer.parseInt(request.getParameter("productStocks"));
             
+            product.setGenre(genre);
+            product.setNumberStocks(numberStocks);
+            product.setPrice(price);
+            product.setSummary(summary);
+            product.setTitle(title);
+            product.setType(type);
+            product.setYear(year);
             
+            boolean addProduct = false;
+            if(type.equals("Audio CD")){
+                AudioCDBean cd = new AudioCDBean();
+                String artist, recordCompany;
+                
+                artist=request.getParameter("cdArtist");
+                recordCompany=request.getParameter("cdRecord");
+                
+                boolean addCD=false;
+                
+                
+                cd.setArtist(artist);
+                cd.setRecordCompany(recordCompany);
+                
+            }
 
         }
     }
